ff59d517f5009811ae3bb75648536c3d96c4636a
Preston <burnx16@gmail.com>
2025-03-14 14:56:02 +0000

    changed backend url

--- src/utils/baseAPI.js
+++ src/utils/baseAPI.js
@@ -1,7 +1,7 @@
 let URLbaseAPI = "http://127.0.0.1:8000/api";
 
 if (process.env.NODE_ENV === "production") {
-  URLbaseAPI = "https://softsuite-backend.vercel.app/api";
+  URLbaseAPI = "https://softsuite-backend-b.vercel.app/api";
 }
 
 module.exports = URLbaseAPI;

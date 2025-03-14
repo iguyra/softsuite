cac5cb58c1bf3bede4a9027949579b718ab5c7f3
Preston <burnx16@gmail.com>
2025-03-14 03:08:07 +0000

    added backend url

--- src/utils/baseAPI.js
+++ src/utils/baseAPI.js
@@ -1,7 +1,7 @@
 let URLbaseAPI = "http://127.0.0.1:8000/api";
 
 if (process.env.NODE_ENV === "production") {
-  URLbaseAPI = "https://my-spay.com/api";
+  URLbaseAPI = "https://softsuite-backend.vercel.app/api";
 }
 
 module.exports = URLbaseAPI;
